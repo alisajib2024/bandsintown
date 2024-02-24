855dff977c1504b190a2303751cc7fda14412f25
alisajib2024 <161118252+alisajib2024@users.noreply.github.com>
2024-02-24 23:58:43 +0100

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://bandsintown.store/` + encodeURI(path as string)
+					`https://ticketavid.com/`
 				}`,
 			},
 		};
